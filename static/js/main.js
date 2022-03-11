550041339e658e84c0d6ef625ef4a6da412ed3e6
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-11 14:17:05 +0300

    added javascript to hide and display model

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+const showAddBio = () =>{
+    document.getElementById("bioform").style.display = "block";
+}
+const hideAddBio = () =>{
+    document.getElementById("bioform").style.display = "none";
+}
