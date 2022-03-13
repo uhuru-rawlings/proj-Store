5e3b51e8b023f696d1c6c46ce9bceeed8c98b0d7
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-13 12:51:48 +0300

    added rating model

--- static/js/models.js
+++ static/js/models.js
@@ -1,3 +1,11 @@
 const showModels = (clicked_id) =>{
     document.getElementsByClassName(clicked_id)[0].style.display = "block";
 }
+
+const hideratings = () =>{
+    document.getElementById("retinmgsform").style.display = "none";
+}
+const showratings = (clicked_ids) =>{
+    document.getElementById("retinmgsform").style.display = "block";
+    document.getElementById("retedpost").value = clicked_ids;
+}
